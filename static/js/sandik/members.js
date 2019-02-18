e656e1d3ef95d08275468271c525a2b5ada77b90
yilmazm <yilmazmu15@itu.edu.tr>
2019-02-18 23:30:22 +0300

    Fix overflow-x in share-table-div in members page

--- static/js/sandik/members.js
+++ static/js/sandik/members.js
@@ -3,3 +3,19 @@ $(function () {
         $(this).next().toggle()
     });
 });
+
+$(window).resize(function() {
+    ChangeWidth();
+});
+
+function ChangeWidth() {
+  if($( window ).width() < 750){
+    $(".share-table-div").css({"width": $( window ).width()-50 });
+  }
+  else{
+    $(".share-table-div").css({"width": "100%" });
+  }
+  //alert($( window ).width());
+}
+
+window.onload = ChangeWidth
